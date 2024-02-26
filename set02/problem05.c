2ca5e04c7ff71e3a46fce49acdc9f1955903e56c
Naveed Feroz <naveedf.bsc23@rvu.edu.in>
2024-02-26 23:57:43 +0530

    26/02

---
+++ set02/problem05.c
@@ -0,0 +1,28 @@
+//GCD 
+
+#include <stdio.h>
+int input();
+int find_gcd(int a, int b);
+void output(int a, int b, int gcd);
+
+
+void main() { 
+
+    printf("enetr the two nums");
+    int a = input();
+    int b = input();
+    int gcd = find_gcd(a,b);
+
+   
+
+}
+
+int input() {
+    int a;
+    scanf("%d", &a);
+    return a;
+          
+}
+
+int find_gcd(int a,int b) {
+    if}
