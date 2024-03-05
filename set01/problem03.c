757aa9420ca467b5ea95ca4d84211fef86c46464
Naveed Feroz <naveedf.bsc23@rvu.edu.in>
2024-03-05 11:54:44 +0530

    new com

---
+++ set01/problem03.c
@@ -0,0 +1,29 @@
+int input();
+int add(int a, int b);
+void output(int a, int b, int sum);
+
+#include <stdio.h>
+
+int main() { 
+    printf("input the two numbers ");
+    int a = input();
+    int b = input();
+    int sum = add(a,b);
+    output(a,b,sum);
+
+}
+int add(int a, int b) {
+    int sum = a+b;
+    return sum;
+}
+
+int input() {
+    int a;
+    scanf("%d", &a);
+    return a;
+}
+
+void output(int a,int b, int sum) {
+
+    printf("the sum of %d and %d is %d",a,b,sum);
+}
